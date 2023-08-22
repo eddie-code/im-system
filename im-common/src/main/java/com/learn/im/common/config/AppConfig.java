e79df2b4b2e2ec2ab889ccf91fbf2ae6f5ab564f
eddie-code <231071372@qq.com>
2023-08-22 17:15:40 +0800

    {8.5}-Yaml配置文件方式, 配置负载均衡策略

--- im-common/src/main/java/com/learn/im/common/config/AppConfig.java
+++ im-common/src/main/java/com/learn/im/common/config/AppConfig.java
@@ -23,4 +23,14 @@ public class AppConfig {
      */
     private Integer zkConnectTimeOut;
 
+    /**
+     * im管道地址路由策略
+     */
+    private Integer imRouteWay;
+
+    /**
+     * 如果选用一致性hash的话具体hash算法
+     */
+    private Integer consistentHashWay;
+
 }
--- im-service/src/main/java/com/learn/im/service/config/BeanConfig.java
+++ im-service/src/main/java/com/learn/im/service/config/BeanConfig.java
@@ -1,7 +1,10 @@
 package com.learn.im.service.config;
 
 import com.learn.im.common.config.AppConfig;
+import com.learn.im.common.enums.ImUrlRouteWayEnum;
+import com.learn.im.common.enums.RouteHashMethodEnum;
 import com.learn.im.common.route.RouteHandle;
+import com.learn.im.common.route.algorithm.consistenthash.AbstractConsistentHash;
 import com.learn.im.common.route.algorithm.consistenthash.ConsistentHashHandle;
 import com.learn.im.common.route.algorithm.consistenthash.TreeMapConsistentHash;
 import com.learn.im.common.route.algorithm.loop.LoopHandle;
@@ -10,6 +13,8 @@ import org.I0Itec.zkclient.ZkClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
 
 /**
  * @description:
@@ -37,12 +42,46 @@ public class BeanConfig {
 //        return new LoopHandle();
 //    }
 
+//    @Bean
+//    public RouteHandle routeHandle() {
+//        ConsistentHashHandle consistentHashHandle = new ConsistentHashHandle();
+//        TreeMapConsistentHash treeMapConsistentHash = new TreeMapConsistentHash();
+//        consistentHashHandle.setHash(treeMapConsistentHash);
+//        return consistentHashHandle;
+//    }
+
     @Bean
-    public RouteHandle routeHandle() {
-        ConsistentHashHandle consistentHashHandle = new ConsistentHashHandle();
-        TreeMapConsistentHash treeMapConsistentHash = new TreeMapConsistentHash();
-        consistentHashHandle.setHash(treeMapConsistentHash);
-        return consistentHashHandle;
+    public RouteHandle routeHandle() throws Exception {
+
+        // 1. 读取配置信息，获取IM路由方式的值
+        Integer imRouteWay = appConfig.getImRouteWay();
+        String routWay = "";
+
+        // 2. 根据枚举值获取对应的绝对路径的类名
+        ImUrlRouteWayEnum handler = ImUrlRouteWayEnum.getHandler(imRouteWay);
+        routWay = handler.getClazz();
+
+        // 3. 使用反射创建该类的实例。
+        RouteHandle routeHandle = (RouteHandle) Class.forName(routWay).newInstance();
+
+        // 4. 如果路由方式是一致性哈希
+        if (handler == ImUrlRouteWayEnum.HASH) {
+            // a. 反射获取设置哈希方式的方法名称
+            Method setHash = Class.forName(routWay).getMethod("setHash", AbstractConsistentHash.class);
+            // b. 读取配置信息，获取一致性哈希方式的值
+            Integer consistentHashWay = appConfig.getConsistentHashWay();
+            String hashWay = "";
+            // c. 根据枚举值获取对应的绝对路径的类名
+            RouteHashMethodEnum hashHandler = RouteHashMethodEnum.getHandler(consistentHashWay);
+            assert hashHandler != null;
+            // d. 使用反射创建该类的实例
+            hashWay = hashHandler.getClazz();
+            AbstractConsistentHash consistentHash = (AbstractConsistentHash) Class.forName(hashWay).newInstance();
+            // e. 调用设置哈希方式的方法，将一致性哈希实例设置到路由处理器中
+            setHash.invoke(routeHandle, consistentHash);
+        }
+        // 5. 返回路由处理器实例
+        return routeHandle;
     }
 
 }
