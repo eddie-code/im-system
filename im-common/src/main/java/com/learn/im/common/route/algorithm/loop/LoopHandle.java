18953882d997bfe26bc831ce0b17feb751e679d8
eddie-code <231071372@qq.com>
2023-08-22 14:18:53 +0800

    {8.3}-负载均衡策略-轮训模式

---
+++ im-common/src/main/java/com/learn/im/common/route/algorithm/loop/LoopHandle.java
@@ -0,0 +1,30 @@
+package com.learn.im.common.route.algorithm.loop;
+
+import com.learn.im.common.enums.UserErrorCode;
+import com.learn.im.common.exception.ApplicationException;
+import com.learn.im.common.route.RouteHandle;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author lee
+ * @description
+ */
+public class LoopHandle implements RouteHandle {
+
+    private final AtomicLong index = new AtomicLong();
+
+    @Override
+    public String routeServer(List<String> values, String key) {
+        int size = values.size();
+        if (size == 0) {
+            throw new ApplicationException(UserErrorCode.SERVER_NOT_AVAILABLE);
+        }
+        Long l = index.incrementAndGet() % size;
+        if (l < 0) {
+            l = 0L;
+        }
+        return values.get(l.intValue());
+    }
+}
--- im-service/src/main/java/com/learn/im/service/config/BeanConfig.java
+++ im-service/src/main/java/com/learn/im/service/config/BeanConfig.java
@@ -2,6 +2,9 @@ package com.learn.im.service.config;
 
 import com.learn.im.common.config.AppConfig;
 import com.learn.im.common.route.RouteHandle;
+import com.learn.im.common.route.algorithm.consistenthash.ConsistentHashHandle;
+import com.learn.im.common.route.algorithm.consistenthash.TreeMapConsistentHash;
+import com.learn.im.common.route.algorithm.loop.LoopHandle;
 import com.learn.im.common.route.algorithm.random.RandomHandle;
 import org.I0Itec.zkclient.ZkClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +27,22 @@ public class BeanConfig {
         return new ZkClient(appConfig.getZkAddr(), appConfig.getZkConnectTimeOut());
     }
 
+//    @Bean
+//    public RouteHandle routeHandle() {
+//        return new RandomHandle();
+//    }
+
     @Bean
     public RouteHandle routeHandle() {
-        return new RandomHandle();
+        return new LoopHandle();
     }
+
+//    @Bean
+//    public RouteHandle routeHandle() {
+//        ConsistentHashHandle consistentHashHandle = new ConsistentHashHandle();
+//        TreeMapConsistentHash treeMapConsistentHash = new TreeMapConsistentHash();
+//        consistentHashHandle.setHash(treeMapConsistentHash);
+//        return consistentHashHandle;
+//    }
+
 }
