328ddfca8014ba1ab3bfa835d6eca852663f9c43
eddie-code <231071372@qq.com>
2023-09-04 14:51:11 +0800

    {8.14}-封装MessageProducer给用户发送消息

---
+++ im-service/src/main/java/com/learn/im/service/utils/MessageProducer.java
@@ -0,0 +1,113 @@
+package com.learn.im.service.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.learn.im.codec.proto.MessagePack;
+import com.learn.im.common.enums.command.Command;
+import com.learn.im.common.model.ClientInfo;
+import com.learn.im.common.model.UserSession;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author lee
+ * @description
+ */
+@Slf4j
+@Service
+public class MessageProducer {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    UserSessionUtils userSessionUtils;
+
+    public boolean sendMessage(UserSession session, Object msg) {
+        try {
+            log.info("send message == " + msg);
+            // BrokerId 区别那个服务器id
+            rabbitTemplate.convertAndSend("", session.getBrokerId() + "", msg);
+            return true;
+        } catch (Exception e) {
+            log.error("send error :" + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 包装数据，调用sendMessage
+     */
+    public boolean sendPack(String toId, Command command, Object msg, UserSession session) {
+        // 路由属性（发送给谁）
+        MessagePack messagePack = new MessagePack();
+        messagePack.setCommand(command.getCommand());
+        messagePack.setToId(toId);
+        messagePack.setClientType(session.getClientType());
+        messagePack.setAppId(session.getAppId());
+        messagePack.setImei(session.getImei());
+        // 真正需要发送客户端的数据
+        JSONObject jsonObject = JSONObject.parseObject(JSONObject.toJSONString(msg));
+        messagePack.setData(jsonObject);
+        // 推到MQ
+        String body = JSONObject.toJSONString(messagePack);
+        return sendMessage(session, body);
+    }
+
+    /**
+     * 发送给所有端的方法
+     */
+    public List<ClientInfo> sendToUser(String toId, Command command, Object data, Integer appId) {
+        List<UserSession> userSession = userSessionUtils.getUserSession(appId, toId);
+        List<ClientInfo> list = new ArrayList<>();
+        for (UserSession session : userSession) {
+            boolean b = sendPack(toId, command, data, session);
+            if (b) {
+                list.add(new ClientInfo(session.getAppId(), session.getClientType(), session.getImei()));
+            }
+        }
+        return list;
+    }
+
+    public void sendToUser(String toId, Integer clientType, String imei, Command command, Object data, Integer appId) {
+        if (clientType != null && StringUtils.isNotBlank(imei)) {
+            ClientInfo clientInfo = new ClientInfo(appId, clientType, imei);
+            sendToUserExceptClient(toId, command, data, clientInfo);
+        } else {
+            sendToUser(toId, command, data, appId);
+        }
+    }
+
+    /**
+     * 发送给某个用户的指定客户端
+     */
+    public void sendToUser(String toId, Command command, Object data, ClientInfo clientInfo) {
+        UserSession userSession = userSessionUtils.getUserSession(clientInfo.getAppId(), toId, clientInfo.getClientType(), clientInfo.getImei());
+        sendPack(toId, command, data, userSession);
+    }
+
+    private boolean isMatch(UserSession sessionDto, ClientInfo clientInfo) {
+        return Objects.equals(sessionDto.getAppId(), clientInfo.getAppId())
+                && Objects.equals(sessionDto.getImei(), clientInfo.getImei())
+                && Objects.equals(sessionDto.getClientType(), clientInfo.getClientType());
+    }
+
+    /**
+     * 发送给除了某一端的其他端
+     */
+    public void sendToUserExceptClient(String toId, Command command, Object data, ClientInfo clientInfo) {
+        List<UserSession> userSession = userSessionUtils.getUserSession(clientInfo.getAppId(), toId);
+        for (UserSession session : userSession) {
+            if (!isMatch(session, clientInfo)) {
+                sendPack(toId, command, data, session);
+            }
+        }
+    }
+
+}
