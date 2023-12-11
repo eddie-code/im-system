10f65883dbad71aa2e8f59e065fb67287d5d62f4
eddie-code <231071372@qq.com>
2023-12-11 15:39:58 +0800

    {9.4}-补充配置2

--- im-service/src/main/java/com/learn/im/service/message/mq/ChatOperateReceiver.java
+++ im-service/src/main/java/com/learn/im/service/message/mq/ChatOperateReceiver.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.learn.im.common.constant.Constants;
 import com.learn.im.common.enums.command.MessageCommand;
-import com.learn.im.service.message.service.P2PMessageService;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
@@ -12,12 +11,10 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import javax.xml.ws.handler.MessageContext;
 import java.util.Map;
 
 /**
@@ -27,9 +24,6 @@ import java.util.Map;
 @Slf4j
 @Component
 public class ChatOperateReceiver {
-
-    @Autowired
-    P2PMessageService p2PMessageService;
 
     @RabbitListener(
             bindings = @QueueBinding(
@@ -49,8 +43,7 @@ public class ChatOperateReceiver {
             Integer command = jsonObject.getInteger("command");
             // 单聊消息 1103
             if (command.equals(MessageCommand.MSG_P2P.getCommand())) {
-                MessageContext messageContext = jsonObject.toJavaObject(MessageContext.class);
-
+                // 处理消息
             }
 
 
