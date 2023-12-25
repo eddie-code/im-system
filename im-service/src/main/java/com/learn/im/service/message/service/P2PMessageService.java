8cb205f0d02ea8b1d375c122dd1dea95c6ce1039
eddie-code <231071372@qq.com>
2023-12-25 23:54:37 +0800

    {10.2}-消息实时性-利用多线程解决消息串行问题，提升处理效率

--- im-service/src/main/java/com/learn/im/service/message/service/P2PMessageService.java
+++ im-service/src/main/java/com/learn/im/service/message/service/P2PMessageService.java
@@ -1,6 +1,5 @@
 package com.learn.im.service.message.service;
 
-import cn.hutool.core.bean.BeanUtil;
 import com.learn.im.codec.pack.message.ChatMessageAck;
 import com.learn.im.common.ResponseVO;
 import com.learn.im.common.enums.command.MessageCommand;
@@ -16,6 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author lee
@@ -34,6 +38,23 @@ public class P2PMessageService {
     @Autowired
     MessageStoreService messageStoreService;
 
+    private final ThreadPoolExecutor threadPoolExecutor;
+
+    {
+        AtomicInteger num = new AtomicInteger();
+        threadPoolExecutor = new ThreadPoolExecutor(8, 8, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1000), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                // 设置守护线程;
+                // 将一个用户线程设置为守护线程的方式是在 线程对象创建 之前 用线程对象的setDaemon方法。
+                thread.setDaemon(true);
+                thread.setName("message-process-thread-" + num.getAndIncrement());
+                return thread;
+            }
+        });
+    }
+
     public void process(MessageContent messageContent) {
 
         String fromId = messageContent.getFromId();
@@ -44,14 +65,16 @@ public class P2PMessageService {
         //发送方和接收方是否是好友
         ResponseVO responseVO = imServerPermissionCheck(fromId, toId, appId);
         if (responseVO.isOk()) {
-            // 插入数据
-            messageStoreService.storeP2PMessage(messageContent);
-            // 1、回ack成功给自己
-            ack(messageContent, ResponseVO.successResponse());
-            // 2、发送消息给同步在线端
-            syncToSender(messageContent, messageContent);
-            // 3、发送消息给对方在线端
-            dispatchMessage(messageContent);
+            threadPoolExecutor.execute(() -> {
+                // 插入数据
+                messageStoreService.storeP2PMessage(messageContent);
+                // 1、回ack成功给自己
+                ack(messageContent, ResponseVO.successResponse());
+                // 2、发送消息给同步在线端
+                syncToSender(messageContent, messageContent);
+                // 3、发送消息给对方在线端
+                dispatchMessage(messageContent);
+            });
         } else {
             // 告诉客户端失败了
             // ack
